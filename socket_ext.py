e9fa5901a6741841b650ddd88577feb5a85bc7c3
Henry Wang <wanghenry1998@gmail.com>
2019-03-29 14:28:55 -0500

    Add socket ext: recvn

---
+++ socket_ext.py
@@ -0,0 +1,21 @@
+# Extends the socket class by implementing a few new methods
+# Just do `import socket_ext` and you socket should be updated!
+
+from socket import *
+
+def recvn(sock, size):
+    """ 
+    Receives EXACTLY <size> bytes. If this cannot be done, this method will
+    raise EOFError exception.
+    """
+
+    res = b''
+    while size > 0:
+        data = sock.recv(size)
+        if data == b'':
+            raise EOFError()
+        size -= len(data)
+        res += data
+    return res
+
+socket.recvn = recvn
